d4d626ee260606163989485fc96ac35f6227d8a9
Your Name <you@example.com>
2024-03-04 20:46:32 +0300

    ss

--- main.py
+++ main.py
@@ -2,15 +2,52 @@ class Student:
     def __init__(self, name: str, marks: list[int]):
         self._name = name
         self._marks = marks
+
     def get_marks(self) -> list[int]:
         return self._marks
+
     def add_mark(self, mark: int):
         self._marks.append(mark)
+
     def avg_mark(self):
         return sum(self._marks) / len(self._marks)
+
     def __str__(self):
         return f'{self._name}'
 
+    def __gt__(self, other):
+        return self.avg_mark() > other.avg_mark()
+
+    def __lt__(self, other):
+        return self.avg_mark() < other.avg_mark()
+
+
+class Group:
+    def __init__(self, students: list[Student] | None = None):
+        if students is None:
+            self._students = []
+        else:
+            self._students = students
+
+    def __iter__(self):
+        return iter(self._students)
+
+    def __len__(self):
+        return len(self._students)
+
+    def __contains__(self, student: str):
+        return student in map(lambda x: str(x), self._students)
+
+    def avg_mark(self):
+        pass
+
+
+
+s1 = Student('John', [5, 5, 5, 5])
+s2 = Student('Michael', [3, 4, 5])
+s3 = Student('Joe', [4, 3, 4])
+g = Group([s1, s2, s3])
+print(max(g))
 # class Dollar:
 #     def __init__(self, dollars: int, cents: int):
 #         self._dollars = dollars
